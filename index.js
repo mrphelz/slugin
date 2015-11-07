9a5d91c51452b9b7d1e11bcf9eebd3a676940199
Alain Beauvois <alain@itipic.com>
2015-11-07 02:10:19 +0100

    Fix error : Uncaught TypeError: cb is not a function
    Mongoose: 4.2.4
    Node: 5.0.0
    
    In some case the method schema.methods.save is called with 2 arguments,
    causing the bug. In this case, the first argument always contains this :
     { __noPromise: true }.

--- index.js
+++ index.js
@@ -75,7 +75,8 @@ function Slugin(schema, options) {
     next();
   });
 
-  schema.methods.save = function(cb){
+  schema.methods.save = function(foo, cb){
+    if (!cb) cb = foo;
     var self = this;
     mongoose.Model.prototype.save.call(self, function(e, model, num) {
       if (e && (e.code === 11000  || e.code === 11001) && !!~e.errmsg.indexOf(self[options.slugName])) {
